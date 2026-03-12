eeb4d7297d7c9d2d25298f54ad0080e21a257536
Newton-14 <onyangonewcienton@gmail.com>
2026-03-12 13:28:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import streamlit as st
+import cv2
+import numpy as np
 from PIL import Image
 import random
 
 st.title("AI Fashion Assistant 👗")
 
-st.write("Upload a photo and get fashion suggestions.")
+st.write("Upload your photo and AI will detect your face.")
 
 uploaded_file = st.file_uploader("Upload your photo", type=["jpg","png","jpeg"])
 
@@ -26,19 +28,37 @@ outfits = [
 
 makeup = [
 "Natural glow",
-"Matte finish",
 "Soft glam",
-"Bold lipstick look",
-"Minimal clean makeup"
+"Matte finish",
+"Bold lipstick",
+"Minimal makeup"
 ]
 
 if uploaded_file is not None:
+
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Photo", use_column_width=True)
+    img = np.array(image)
 
-    if st.button("Generate Style"):
-        st.subheader("AI Suggestions")
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        st.write("Hairstyle:", random.choice(hairstyles))
-        st.write("Outfit:", random.choice(outfits))
-        st.write("Makeup:", random.choice(makeup))
+    face_cascade = cv2.CascadeClassifier(
+        cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+    )
+
+    faces = face_cascade.detectMultiScale(gray,1.3,5)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+
+    st.image(img, caption="Detected Face", use_column_width=True)
+
+    if len(faces) > 0:
+        if st.button("Generate AI Style"):
+            st.subheader("AI Fashion Suggestions")
+
+            st.write("Hairstyle:", random.choice(hairstyles))
+            st.write("Outfit:", random.choice(outfits))
+            st.write("Makeup:", random.choice(makeup))
+
+    else:
+        st.warning("No face detected. Try another image.")
